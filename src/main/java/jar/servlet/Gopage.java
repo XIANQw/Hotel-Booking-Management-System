01af3d0e2e479df327d6327dd04c5b97410348ef
ruiwen29 <36294327+ruiwen29@users.noreply.github.com>
2020-04-12 11:57:14 +0200

    add gopage Modifyaccount

--- src/main/java/jar/servlet/Gopage.java
+++ src/main/java/jar/servlet/Gopage.java
@@ -2,15 +2,11 @@ package jar.servlet;
 
 import javax.servlet.http.*;
 
-import jar.bean.UserBean;
-import jar.dao.UserDao;
-
 import java.io.IOException;
-import java.util.HashMap;
 
 import javax.servlet.*;
 
-public class Gotopage extends HttpServlet{
+public class Gopage extends HttpServlet{
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws IOException, ServletException{
         doPost(req, resp);
@@ -18,28 +14,54 @@ public class Gotopage extends HttpServlet{
 
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
     throws IOException, ServletException{
-		String page = req.getParameter("page");
-		if ("mainPage".equals(page)) {
-            mainPgae(req, resp);
-		} else if ("profile".equals(page)) {
-			      profile(req, resp);
+        String page = req.getParameter("page");
+        if("accueil".equals(page)){
+            accueil(req, resp);
+        }
+      else if ("mainPage".equals(page)) {
+            mainPage(req, resp);
 		}
+      else if ("profile".equals(page)) {
+			profile(req, resp);
+		}
+      else if("modifyAccount".equals(page)){
+      modifyAccount(req,resp);
+    }
       else {
-			return ;
-		}
+			     return ;
+		  }
     }
 
-    public void mainPage(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
-            req.getRequestDispatcher("/static/view/main.jsp").forward(req, resp);
-            return ;
+    public void accueil(HttpServletRequest req, HttpServletResponse resp)
+    throws IOException, ServletException{
+        req.getRequestDispatcher("/static/view/accueil.jsp").forward(req, resp);
     }
-    public void profile(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+
+    public void mainPage(HttpServletRequest req, HttpServletResponse resp)
+    throws IOException, ServletException{
+        if(!Client.sessionValide(req, resp)){
+            accueil(req, resp);
+        } else{
+            req.getRequestDispatcher("/static/view/mainPage.jsp").forward(req, resp);
+        }
+    }
+
+    public void profile(HttpServletRequest req, HttpServletResponse resp)
+    throws IOException, ServletException{
+        if(!Client.sessionValide(req, resp)){
+            accueil(req, resp);
+        } else{
             req.getRequestDispatcher("/static/view/profile.jsp").forward(req, resp);
-            return ;
+        }
     }
 
-
-
-
+    public void modifyAccount(HttpServletRequest req, HttpServletResponse resp)
+    throws IOException, ServletException{
+        if(!Client.sessionValide(req, resp)){
+            accueil(req, resp);
+        } else{
+            req.getRequestDispatcher("/static/view/modifyAccount.jsp").forward(req, resp);
+        }
+    }
 
 }
